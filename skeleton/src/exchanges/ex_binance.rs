44310fb2740174c6b114ef6418932775de5aa259
unkuseni <amudalatadeoye@gmail.com>
2024-03-13 17:53:23 +0100

    binance bba issue solved

--- skeleton/src/exchanges/ex_binance.rs
+++ skeleton/src/exchanges/ex_binance.rs
@@ -1,20 +1,46 @@
+use std::collections::VecDeque;
 use std::sync::atomic::AtomicBool;
+use std::thread;
+use std::time::Duration;
 
-use binance::account::Account;
-use binance::futures::account::{self, FuturesAccount};
+use binance::futures::account::FuturesAccount;
 use binance::futures::model::AccountBalance;
-use binance::model::AccountInformation;
+use binance::model::{
+    Asks, Bids, BookTickerEvent, ContinuousKline, DepthOrderBookEvent, LiquidationOrder, OrderBook,
+};
 use binance::{api::Binance, futures::websockets::*, general::General, model::AggrTradesEvent};
 use tokio::sync::mpsc;
 
-use super::ex_bybit::{BybitMarket, BybitPrivate};
-use super::exchange::Exchange;
+use crate::util::localorderbook::{LocalBook, ProcessAsks, ProcessBids};
+
+use super::ex_bybit::BybitPrivate;
+#[derive(Clone, Debug)]
+pub struct BinanceMarket {
+    pub time: u64,
+    pub books: Vec<(String, LocalBook)>,
+    pub klines: Vec<(String, VecDeque<ContinuousKline>)>,
+    pub trades: Vec<(String, VecDeque<AggrTradesEvent>)>,
+    pub tickers: Vec<(String, VecDeque<BookTickerEvent>)>,
+    pub liquidations: Vec<(String, VecDeque<LiquidationOrder>)>,
+}
+
+impl Default for BinanceMarket {
+    fn default() -> Self {
+        Self {
+            time: 0,
+            books: Vec::new(),
+            klines: Vec::new(),
+            trades: Vec::new(),
+            tickers: Vec::new(),
+            liquidations: Vec::new(),
+        }
+    }
+}
 #[derive(Clone, Debug, PartialEq)]
 pub struct BinanceClient {
     pub key: String,
     pub secret: String,
 }
-
 impl BinanceClient {
     pub fn init(key: String, secret: String) -> Self {
         Self { key, secret }
@@ -34,23 +60,170 @@ impl BinanceClient {
         }
     }
 
-    pub async fn market_subscribe(
+    pub fn market_subscribe(
         &self,
         symbol: Vec<&str>,
-        sender: mpsc::UnboundedSender<BybitMarket>,
+        sender: mpsc::UnboundedSender<BinanceMarket>,
     ) {
-        unimplemented!()
-    }
+        let mut delay = 600;
+        let keep_running = AtomicBool::new(true);
+        let request = bin_build_requests(&symbol);
 
+        let mut market_data = BinanceMarket::default();
+        market_data.books = symbol
+            .iter()
+            .map(|s| (s.to_string(), LocalBook::new()))
+            .collect::<Vec<(String, LocalBook)>>();
+        market_data.klines = symbol
+            .iter()
+            .map(|s| (s.to_string(), VecDeque::with_capacity(2000)))
+            .collect::<Vec<(String, VecDeque<ContinuousKline>)>>();
+        market_data.liquidations = symbol
+            .iter()
+            .map(|s| (s.to_string(), VecDeque::with_capacity(2000)))
+            .collect::<Vec<(String, VecDeque<LiquidationOrder>)>>();
+        market_data.trades = symbol
+            .iter()
+            .map(|s| (s.to_string(), VecDeque::with_capacity(5000)))
+            .collect::<Vec<(String, VecDeque<AggrTradesEvent>)>>();
+        market_data.tickers = symbol
+            .iter()
+            .map(|s| (s.to_string(), VecDeque::with_capacity(10)))
+            .collect::<Vec<(String, VecDeque<BookTickerEvent>)>>();
+
+        let handler = move |event| {
+            match event {
+                FuturesWebsocketEvent::DepthOrderBook(DepthOrderBookEvent {
+                    symbol,
+                    event_time,
+                    bids,
+                    asks,
+                    ..
+                }) => {
+                    let sym = symbol.as_str();
+                    let book = &mut market_data
+                        .books
+                        .iter_mut()
+                        .find(|(s, _)| s == sym)
+                        .unwrap()
+                        .1;
+                    let new_bids = {
+                        let mut arr = Vec::new();
+                        for bid in bids {
+                            arr.push(Bids::process_bids(bid));
+                        }
+                        arr
+                    };
+                    let new_asks = {
+                        let mut arr = Vec::new();
+                        for ask in asks {
+                            arr.push(Asks::process_asks(ask));
+                        }
+                        arr
+                    };
+                    if new_bids.len() == new_asks.len()
+                        && (new_bids.len() == 5 || new_bids.len() == 10 || new_bids.len() == 20)
+                    {
+                        // Process when the lengths are equal and equal to 5, 10, or 20
+                        book.update_binance_bba(new_bids.clone(), new_asks.clone(), event_time);
+                    } else {
+                        // Process when the lengths are not equal or not equal to 5, 10, or 20
+                        book.update(new_bids.clone(), new_asks.clone(), event_time);
+                    }
+                    sender.send(market_data.clone()).unwrap();
+                    market_data.time = event_time;
+                }
+                FuturesWebsocketEvent::AggrTrades(agg) => {
+                    let sym = agg.symbol.as_str();
+                    let trades = &mut market_data
+                        .trades
+                        .iter_mut()
+                        .find(|(s, _)| s == sym)
+                        .unwrap()
+                        .1;
+                    if trades.len() == trades.capacity() || (trades.capacity() - trades.len()) <= 5
+                    {
+                        for _ in 0..10 {
+                            trades.pop_front();
+                        }
+                    }
+                    trades.push_back(agg);
+                }
+                FuturesWebsocketEvent::Liquidation(liquidation) => {
+                    let sym = liquidation.liquidation_order.symbol.as_str();
+                    let liquidations = &mut market_data
+                        .liquidations
+                        .iter_mut()
+                        .find(|(s, _)| s == sym)
+                        .unwrap()
+                        .1;
+                    if liquidations.len() == liquidations.capacity()
+                        || (liquidations.capacity() - liquidations.len()) <= 5
+                    {
+                        for _ in 0..10 {
+                            liquidations.pop_front();
+                        }
+                    }
+                    liquidations.push_back(liquidation.liquidation_order);
+                }
+                FuturesWebsocketEvent::ContinuousKline(kline) => {
+                    let sym = kline.pair.as_str();
+                    let kline_data = &mut market_data
+                        .klines
+                        .iter_mut()
+                        .find(|(s, _)| s == sym)
+                        .unwrap()
+                        .1;
+                    if kline_data.len() == kline_data.capacity()
+                        || (kline_data.capacity() - kline_data.len()) <= 10
+                    {
+                        for _ in 0..30 {
+                            kline_data.pop_front();
+                        }
+                    }
+                    kline_data.push_back(kline.kline);
+                }
+
+                FuturesWebsocketEvent::BookTicker(ticker) => {
+                    let sym = ticker.symbol.as_str();
+                    let ticker_data = &mut market_data
+                        .tickers
+                        .iter_mut()
+                        .find(|(s, _)| s == sym)
+                        .unwrap()
+                        .1;
+                    if ticker_data.len() == ticker_data.capacity()
+                        || (ticker_data.capacity() - ticker_data.len()) <= 10
+                    {
+                        for _ in 0..10 {
+                            ticker_data.pop_front();
+                        }
+                    }
+                    ticker_data.push_back(ticker);
+                }
+                _ => {}
+            }
+            Ok(())
+        };
+
+        let mut market: FuturesWebSockets<'_> = FuturesWebSockets::new(handler);
+        loop {
+            market
+                .connect_multiple_streams(&FuturesMarket::USDM, &request)
+                .unwrap();
+            // check error
+            if let Err(e) = market.event_loop(&keep_running) {
+                eprintln!("Error: {}", e);
+                thread::sleep(Duration::from_millis(delay));
+                delay *= 2;
+            }
+        }
+    }
     pub async fn private_subscribe(&self, _sender: mpsc::UnboundedSender<BybitPrivate>) {
         unimplemented!()
     }
 
-    pub async fn fee_rate(&self, _symbol: &str) -> f64 {
-        unimplemented!()
-    }
-
-    pub fn fee_rate2(&self, _symbol: &str) -> Vec<AccountBalance> {
+    pub fn fee_rate(&self, _symbol: &str) -> Vec<AccountBalance> {
         let client: FuturesAccount =
             Binance::new(Some(self.key.clone()), Some(self.secret.clone()));
 
@@ -62,54 +235,7 @@ impl BinanceClient {
             unimplemented!()
         }
     }
-    pub fn ws_subscribe(&self, sender: mpsc::UnboundedSender<FuturesWebsocketEvent>) {
-        let keep_running = AtomicBool::new(true); // Used to control the event loop
-        let agg_trade = String::from("ethbtc@aggTrade");
-        let mut web_socket: FuturesWebSockets<'_> =
-            FuturesWebSockets::new(|event: FuturesWebsocketEvent| {
-                match event {
-                    FuturesWebsocketEvent::Trade(trade) => {
-                        sender
-                            .send(FuturesWebsocketEvent::Trade(trade.clone()))
-                            .unwrap();
-                        println!(
-                            "Symbol: {}, price: {}, qty: {}",
-                            trade.symbol, trade.price, trade.qty
-                        );
-                    }
-                    FuturesWebsocketEvent::DepthOrderBook(depth_order_book) => {
-                        sender
-                            .send(FuturesWebsocketEvent::DepthOrderBook(
-                                depth_order_book.clone(),
-                            ))
-                            .unwrap();
-                        println!(
-                            "Symbol: {}, Bids: {:?}, Ask: {:?}",
-                            depth_order_book.symbol, depth_order_book.bids, depth_order_book.asks
-                        );
-                    }
-                    FuturesWebsocketEvent::OrderBook(order_book) => {
-                        sender
-                            .send(FuturesWebsocketEvent::OrderBook(order_book.clone()))
-                            .unwrap();
-                        println!(
-                            "last_update_id: {}, Bids: {:?}, Ask: {:?}",
-                            order_book.last_update_id, order_book.bids, order_book.asks
-                        );
-                    }
-                    _ => (),
-                };
 
-                Ok(())
-            });
-
-        web_socket
-            .connect(&FuturesMarket::USDM, &agg_trade)
-            .unwrap(); // check error
-        if let Err(e) = web_socket.event_loop(&keep_running) {
-            println!("Error: {}", e);
-        }
-    }
     pub fn ws_aggtrades(&self, symbol: Vec<&str>, sender: mpsc::UnboundedSender<AggrTradesEvent>) {
         let keep_running = AtomicBool::new(true); // Used to control the event loop
         let agg_trades: Vec<String> = symbol
@@ -139,4 +265,79 @@ impl BinanceClient {
             println!("Error: {}", e);
         }
     }
+    pub fn ws_best_book(&self, symbol: Vec<&str>, sender: mpsc::UnboundedSender<AggrTradesEvent>) {
+        let keep_running = AtomicBool::new(true); // Used to control the event loop
+        let best_book: Vec<String> = symbol
+            .iter()
+            .map(|&sub| sub.to_lowercase())
+            .flat_map(|sym| vec![("5", sym.clone()), ("10", sym.clone()), ("20", sym.clone())])
+            .map(|(depth, sub)| format!("{}@depth{}@100ms", sub, depth))
+            .collect();
+        let mut web_socket = FuturesWebSockets::new(|event: FuturesWebsocketEvent| {
+            if let FuturesWebsocketEvent::AggrTrades(agg) = event {
+                // println!(
+                //     "Symbol: {}, price: {}, qty: {}",
+                //     agg.symbol, agg.price, agg.qty
+                // );
+                sender.send(agg).unwrap();
+            } else {
+                println!("Unexpected event: {:?}", event);
+            }
+
+            Ok(())
+        });
+        web_socket
+            .connect_multiple_streams(&FuturesMarket::USDM, &best_book)
+            .unwrap();
+
+        // check error
+        if let Err(e) = web_socket.event_loop(&keep_running) {
+            println!("Error: {}", e);
+        }
+    }
+}
+
+fn bin_build_requests(symbol: &[&str]) -> Vec<String> {
+    let mut request_args = vec![];
+
+    // Agg Trades request
+    let trade_req: Vec<String> = symbol
+        .iter()
+        .map(|&sub| sub.to_lowercase())
+        .map(|sub| format!("{}@aggTrade", sub))
+        .collect();
+    request_args.extend(trade_req);
+    let kline_req: Vec<String> = symbol
+        .iter()
+        .map(|&sub| sub.to_lowercase())
+        .flat_map(|sym| vec![("5m", sym.clone()), ("1m", sym.clone())])
+        .map(|(interval, sub)| format!("{}_perpetual@@continuousKline_{}", sub, interval))
+        .collect();
+    request_args.extend(kline_req);
+    let best_book: Vec<String> = symbol
+        .iter()
+        .map(|&sub| sub.to_lowercase())
+        .flat_map(|sym| vec![("5", sym.clone()), ("10", sym.clone()), ("20", sym.clone())])
+        .map(|(depth, sub)| format!("{}@depth{}@100ms", sub, depth))
+        .collect();
+    request_args.extend(best_book);
+    let book: Vec<String> = symbol
+        .iter()
+        .map(|&sub| sub.to_lowercase())
+        .map(|sub| format!("{}@depth@250ms", sub))
+        .collect();
+    request_args.extend(book);
+    let tickers: Vec<String> = symbol
+        .iter()
+        .map(|&sub| sub.to_lowercase())
+        .map(|sub| format!("{}@bookTicker", sub))
+        .collect();
+    request_args.extend(tickers);
+    let liq_req: Vec<String> = symbol
+        .iter()
+        .map(|&sub| sub.to_lowercase())
+        .map(|sub| format!("{}@forceOrder", sub))
+        .collect();
+    request_args.extend(liq_req);
+    request_args
 }
