a69401044205e72a376872a912e80838c5202f94
unkuseni <amudalatadeoye@gmail.com>
2024-03-14 05:39:30 +0100

    Refactor logger implementation

--- skeleton/src/util/logger.rs
+++ skeleton/src/util/logger.rs
@@ -1,43 +1,41 @@
 use std::fmt;
 use super::helpers::generate_timestamp;
+
 pub struct Logger;
 
 impl Logger {
-    pub fn time_now() -> String {
-        let now_millis = generate_timestamp();
-        let now = now_millis / 1000; // Convert milliseconds to seconds
-        format!("{:02}:{:02}:{:02}", now / 3600, (now / 60) % 60, now % 60)
+    pub fn log(level: LogLevel, msg: &str) {
+        let now = generate_timestamp() / 1000; // Convert milliseconds to seconds
+        let (h, m, s) = (now / 3600, (now / 60) % 60, now % 60);
+        println!("{:02}:{:02}:{:02} | {:<8} | {}", h, m, s, level, msg);
     }
 
-    pub fn log(&self, level: LogLevel, msg: &str) {
-        println!("{} | {:<8} | {}", Self::time_now(), level, msg);
+    pub fn success(msg: &str) {
+        Self::log(LogLevel::Success, msg);
     }
 
-    pub fn success(&self, msg: &str) {
-        self.log(LogLevel::Success, msg);
+    pub fn info(msg: &str) {
+        Self::log(LogLevel::Info, msg);
     }
 
-    pub fn info(&self, msg: &str) {
-        self.log(LogLevel::Info, msg);
+    pub fn debug(msg: &str) {
+        Self::log(LogLevel::Debug, msg);
     }
 
-    pub fn debug(&self, msg: &str) {
-        self.log(LogLevel::Debug, msg);
+    pub fn warning(msg: &str) {
+        Self::log(LogLevel::Warning, msg);
     }
 
-    pub fn warning(&self, msg: &str) {
-        self.log(LogLevel::Warning, msg);
+    pub fn error(msg: &str) {
+        Self::log(LogLevel::Error, msg);
     }
 
-    pub fn error(&self, msg: &str) {
-        self.log(LogLevel::Error, msg);
-    }
-
-    pub fn critical(&self, msg: &str) {
-        self.log(LogLevel::Critical, msg);
+    pub fn critical(msg: &str) {
+        Self::log(LogLevel::Critical, msg);
     }
 }
 
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub enum LogLevel {
     Success,
     Info,
@@ -49,17 +47,8 @@ pub enum LogLevel {
 
 impl fmt::Display for LogLevel {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "{}",
-            match self {
-                LogLevel::Success => "SUCCESS",
-                LogLevel::Info => "INFO",
-                LogLevel::Debug => "DEBUG",
-                LogLevel::Warning => "WARNING",
-                LogLevel::Error => "ERROR",
-                LogLevel::Critical => "CRITICAL",
-            }
-        )
+        const LEVEL_NAMES: [&str; 6] = ["SUCCESS", "INFO", "DEBUG", "WARNING", "ERROR", "CRITICAL"];
+        let idx = *self as usize;
+        write!(f, "{}", LEVEL_NAMES[idx])
     }
 }
