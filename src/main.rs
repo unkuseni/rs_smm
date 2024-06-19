acf505dc70488366db04961b28259c5cada11c8c
unkuseni <amudalatadeoye@gmail.com>
2024-06-19 04:32:47 +0100

    will be done tomorrow

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use rs_smm::features::engine::Engine;
-use rs_smm::features::imbalance::wmid;
 use std::collections::HashMap;
 
 use skeleton::exchanges::exchange::MarketMessage;
@@ -28,7 +27,7 @@ async fn dub(mut receiver: mpsc::UnboundedReceiver<SharedState>) {
 
     while let Some(v) = receiver.recv().await {
         let bybit_market = &v.markets[0]; // Use reference
-
+        let depth: Vec<usize> = vec![5, 50];
         let trades = match bybit_market {
             MarketMessage::Bybit(b) => &b.trades,
             MarketMessage::Binance(b) => &b.trades,
@@ -50,7 +49,7 @@ async fn dub(mut receiver: mpsc::UnboundedReceiver<SharedState>) {
                 if let (Some(b), Some(t), Some(avg)) =
                     (prev_book, prev_trade, prev_avgs.get(symbol))
                 {
-                    engine.update(book, b, trade, t, avg, Some(5));
+                    engine.update(book, b, trade, t, avg, depth.clone(), 1000);
                 }
 
                 if prev_books.get(symbol).is_none() {
@@ -58,10 +57,13 @@ async fn dub(mut receiver: mpsc::UnboundedReceiver<SharedState>) {
                     prev_avgs.insert(symbol.clone(), engine.avg_trade_price);
                 } else if book.last_update - prev_book.unwrap().last_update >= 100 {
                     println!(
-                    "Symbol: {:#?}, Imb: {:#?}, Voi: {:#?}  Trade_Imb: {:#?} \nPrice_Imp: {:#?}  \nExpected_Ret: {:#?} Price_Flu: {:#?} Expected_Val: {:#?}\nMid_Change: {:#?} Mid_Diff: {:#?} Mid_Avg: {:#?} Mid_Basis: {:#?} \nAvg_Trade_Price: {:#?} Avg_Spread: {:#?}",
-                    symbol, engine.imbalance_ratio, engine.voi, engine.trade_imb, engine.price_impact, engine.expected_return, engine.price_flu, engine.expected_value.1, engine.mid_price_change, engine.mid_price_diff, engine.mid_price_avg, engine.mid_price_basis, engine.avg_trade_price,
-                    engine.avg_spread.1
-                );
+                        "Symbol: {:#?}, Imb: {:#?}, Voi: {:#?}  wmid: {:#?} skew: {:#?}",
+                        symbol,
+                        engine.imbalance_ratio,
+                        engine.voi,
+                        engine.expected_return * 10000.0,
+                        engine.skew
+                    );
                     prev_books.insert(symbol.clone(), book.clone());
                     prev_avgs.insert(symbol.clone(), engine.avg_trade_price);
                 } else {
