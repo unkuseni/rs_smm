d2d34ffd5f2f71690a65c82fae3705b6eadab8c5
unkuseni <amudalatadeoye@gmail.com>
2024-06-20 01:21:13 +0100

    added order functions

--- src/trader/quote_gen.rs
+++ src/trader/quote_gen.rs
@@ -1,7 +1,16 @@
-use std::collections::VecDeque;
+use std::{borrow::Cow, collections::VecDeque};
 
-use binance::futures::account::FuturesAccount;
-use bybit::trade::Trader;
+use binance::{
+    account::OrderSide,
+    futures::account::{CustomOrderRequest, FuturesAccount},
+};
+use bybit::{
+    model::{
+        AmendOrderRequest, BatchAmendRequest, BatchCancelRequest, BatchPlaceRequest,
+        CancelOrderRequest, CancelallRequest, OrderRequest, Side,
+    },
+    trade::Trader,
+};
 use skeleton::{
     exchanges::exchange::{ExchangeClient, PrivateData},
     util::{
@@ -9,7 +18,7 @@ use skeleton::{
         localorderbook::LocalBook,
     },
 };
-use tokio::sync::mpsc::UnboundedReceiver;
+use tokio::{sync::mpsc::UnboundedReceiver, task};
 
 type BybitTrader = Trader;
 type BinanceTrader = FuturesAccount;
@@ -26,33 +35,42 @@ pub struct QuoteGenerator {
     live_buys_orders: VecDeque<LiveOrder>,
     live_sells_orders: VecDeque<LiveOrder>,
     max_position_usd: f64,
+    max_position_qty: f64,
     inventory_delta: f64,
+    total_order: usize,
 }
 
 impl QuoteGenerator {
-    pub fn new(client: ExchangeClient, rounding_value: usize) -> Self {
+    pub fn new(client: ExchangeClient, asset: f64, leverage: f64) -> Self {
         let trader = match client {
             ExchangeClient::Bybit(cl) => OrderManagement::Bybit(cl.bybit_trader()),
             ExchangeClient::Binance(cl) => OrderManagement::Binance(cl.binance_trader()),
         };
         QuoteGenerator {
-            asset: 0.0,
-            rounding_value,
+            asset: asset * leverage,
+            rounding_value: 0,
             client: trader,
             positions: 0.0,
             live_buys_orders: VecDeque::new(),
             live_sells_orders: VecDeque::new(),
             inventory_delta: 0.0,
             max_position_usd: 0.0,
+            max_position_qty: 0.0,
+            total_order: 10,
         }
     }
 
     fn update_max(&mut self) {
-        self.max_position_usd = self.asset / 2.0;
+        self.max_position_usd = self.asset * 0.95;
     }
-    pub fn start_loop(&mut self, mut receiver: UnboundedReceiver<PrivateData>) {}
 
-    fn generate_quotes() {}
+    fn inventory_delta(&mut self, qty: f64, price: f64) {
+        self.inventory_delta = (price * qty) / self.max_position_usd;
+    }
+
+    fn update_max_qty(&mut self, price: f64) {
+        self.max_position_qty = self.max_position_usd / price;
+    }
 
     fn total_orders(&self) -> usize {
         self.live_buys_orders.len() + self.live_sells_orders.len()
@@ -67,26 +85,30 @@ impl QuoteGenerator {
 
     fn adjusted_spread(&self, preferred_spread: f64, book: &LocalBook) -> f64 {
         let min_spread = bps_to_decimal(preferred_spread);
-        book.get_spread()
-            .clip(min_spread, min_spread * 3.7)
+        book.get_spread().clip(min_spread, min_spread * 3.7)
     }
-    
+
+    pub fn start_loop(&mut self, mut receiver: UnboundedReceiver<PrivateData>) {}
+
+    fn generate_quotes() {}
+
+    fn update_orders(&mut self) {}
 }
 
 #[derive(Debug, Clone)]
 pub struct LiveOrder {
     pub price: f64,
     pub qty: f64,
-    pub side: String,
     pub order_id: String,
 }
-
-pub fn inventory_delta(qty: f64, price: f64, max_position: f64) -> f64 {
-    price * qty / max_position
-}
-
-fn max_position(book: &LocalBook, amount: f64) -> f64 {
-    amount / book.mid_price
+impl LiveOrder {
+    pub fn new(price: f64, qty: f64, order_id: String) -> Self {
+        LiveOrder {
+            price,
+            qty,
+            order_id,
+        }
+    }
 }
 
 fn bps_to_decimal(bps: f64) -> f64 {
@@ -112,16 +134,388 @@ fn round_size(price: f64, step: f64) -> f64 {
 
 pub fn liquidate_inventory() {}
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl OrderManagement {
+    async fn place_buy_limit(&self, qty: f64, price: f64, symbol: &str) -> Result<LiveOrder, ()> {
+        match self {
+            OrderManagement::Bybit(trader) => {
+                let client = trader.clone();
 
-    #[test]
-    fn test_inventory_delta() {
-        let qty = 10.0;
-        let price = 1.0;
-        let max_position = 100.0;
-        let delta = inventory_delta(qty, price, max_position);
-        assert_eq!(delta, 0.10);
+                if let Ok(v) = client
+                    .place_futures_limit_order(
+                        bybit::model::Category::Linear,
+                        symbol,
+                        Side::Buy,
+                        qty,
+                        price,
+                        1,
+                    )
+                    .await
+                {
+                    Ok(LiveOrder::new(price, qty, v.result.order_id))
+                } else {
+                    Err(())
+                }
+            }
+            OrderManagement::Binance(trader) => {
+                let symbol = symbol.to_owned();
+                let client = trader.clone();
+                let task = task::spawn_blocking(move || {
+                    if let Ok(v) = client.limit_buy(
+                        symbol,
+                        qty,
+                        price,
+                        binance::futures::account::TimeInForce::GTC,
+                    ) {
+                        Ok(LiveOrder::new(price, qty, v.order_id.to_string()))
+                    } else {
+                        Err(())
+                    }
+                });
+                task.await.unwrap()
+            }
+        }
+    }
+
+    async fn place_sell_limit(&self, qty: f64, price: f64, symbol: &str) -> Result<LiveOrder, ()> {
+        match self {
+            OrderManagement::Bybit(trader) => {
+                let client = trader.clone();
+                if let Ok(v) = client
+                    .place_futures_limit_order(
+                        bybit::model::Category::Linear,
+                        symbol,
+                        Side::Sell,
+                        qty,
+                        price,
+                        2,
+                    )
+                    .await
+                {
+                    Ok(LiveOrder::new(price, qty, v.result.order_id))
+                } else {
+                    Err(())
+                }
+            }
+            OrderManagement::Binance(trader) => {
+                let symbol = symbol.to_owned();
+                let client = trader.clone();
+                let task = task::spawn_blocking(move || {
+                    if let Ok(v) = client.limit_sell(
+                        symbol,
+                        qty,
+                        price,
+                        binance::futures::account::TimeInForce::GTC,
+                    ) {
+                        Ok(LiveOrder::new(price, qty, v.order_id.to_string()))
+                    } else {
+                        Err(())
+                    }
+                });
+                task.await.unwrap()
+            }
+        }
+    }
+
+    async fn amend_order(
+        &self,
+        order: LiveOrder,
+        qty: f64,
+        price: Option<f64>,
+        symbol: &str,
+    ) -> Result<LiveOrder, ()> {
+        match self {
+            OrderManagement::Bybit(trader) => {
+                let client = trader.clone();
+                let req = AmendOrderRequest {
+                    category: bybit::model::Category::Linear,
+                    order_id: Some(Cow::Borrowed(order.order_id.as_str())),
+                    price,
+                    qty,
+                    ..Default::default()
+                };
+                if let Ok(v) = client.amend_order(req).await {
+                    Ok(LiveOrder::new(
+                        price.unwrap_or(order.price),
+                        qty,
+                        v.result.order_id,
+                    ))
+                } else {
+                    Err(())
+                }
+            }
+            OrderManagement::Binance(trader) => {
+                // TODO: binance crate doesn't have an amend_order fn. so this cancels the current and places a new one then returns the new order id
+                let symbol = symbol.to_owned();
+                let client = trader.clone();
+                let task = task::spawn_blocking(move || {
+                    if let Ok(v) =
+                        client.cancel_order(symbol.clone(), order.order_id.parse::<u64>().unwrap())
+                    {
+                        if let Ok(v) = client.limit_sell(
+                            symbol,
+                            qty,
+                            price.unwrap(),
+                            binance::futures::account::TimeInForce::GTC,
+                        ) {
+                            Ok(LiveOrder::new(price.unwrap(), qty, v.order_id.to_string()))
+                        } else {
+                            Err(())
+                        }
+                    } else {
+                        Err(())
+                    }
+                });
+                task.await.unwrap()
+            }
+        }
+    }
+
+    async fn cancel_order(&self, order: LiveOrder, symbol: &str) -> Result<LiveOrder, ()> {
+        match self {
+            OrderManagement::Bybit(trader) => {
+                let client = trader.clone();
+                let req = CancelOrderRequest {
+                    category: bybit::model::Category::Linear,
+                    symbol: Cow::Borrowed(symbol),
+                    order_id: Some(Cow::Borrowed(order.order_id.as_str())),
+                    order_filter: None,
+                    order_link_id: None,
+                };
+                if let Ok(v) = client.cancel_order(req).await {
+                    Ok(LiveOrder::new(order.price, order.qty, v.result.order_id))
+                } else {
+                    Err(())
+                }
+            }
+
+            OrderManagement::Binance(trader) => {
+                let symbol = symbol.to_owned();
+                let client = trader.clone();
+                let task = task::spawn_blocking(move || {
+                    if let Ok(v) =
+                        client.cancel_order(symbol, order.order_id.parse::<u64>().unwrap())
+                    {
+                        Ok(LiveOrder::new(
+                            order.price,
+                            order.qty,
+                            v.order_id.to_string(),
+                        ))
+                    } else {
+                        Err(())
+                    }
+                });
+                task.await.unwrap()
+            }
+        }
+    }
+
+    async fn cancel_all(&self, symbol: &str) -> Result<Vec<LiveOrder>, ()> {
+        let mut arr = vec![];
+        match self {
+            OrderManagement::Bybit(trader) => {
+                let client = trader.clone();
+                let req = CancelallRequest {
+                    category: bybit::model::Category::Linear,
+                    symbol: symbol,
+                    ..Default::default()
+                };
+                if let Ok(v) = client.cancel_all_orders(req).await {
+                    for d in v.result.list {
+                        arr.push(LiveOrder::new(0.0, 0.0, d.order_id));
+                    }
+                    Ok(arr)
+                } else {
+                    Err(())
+                }
+            }
+            OrderManagement::Binance(trader) => {
+                // TODO
+                let symbol = symbol.to_owned();
+                let client = trader.clone();
+                let task = task::spawn_blocking(move || {
+                    if let Ok(v) = client.cancel_all_open_orders(symbol) {
+                        Ok(arr)
+                    } else {
+                        Err(())
+                    }
+                });
+                task.await.unwrap()
+            }
+        }
+    }
+
+    async fn batch_cancel(
+        &self,
+        orders: Vec<LiveOrder>,
+        symbol: &str,
+    ) -> Result<Vec<LiveOrder>, ()> {
+        let mut arr = vec![];
+        match self {
+            OrderManagement::Bybit(trader) => {
+                let client = trader.clone();
+                let req = BatchCancelRequest {
+                    category: bybit::model::Category::Linear,
+                    requests: {
+                        let mut li = vec![];
+                        for v in orders {
+                            let order_id_string = v.order_id.clone();
+                            li.push(CancelOrderRequest {
+                                category: bybit::model::Category::Linear,
+                                symbol: Cow::Borrowed(symbol),
+                                order_id: Some(Cow::Owned(order_id_string)), // Changed to Cow::Owned
+                                order_filter: None,
+                                order_link_id: None,
+                            });
+                        }
+                        li
+                    },
+                };
+                if let Ok(v) = client.batch_cancel_order(req).await {
+                    for d in v.result.list {
+                        arr.push(LiveOrder::new(0.0, 0.0, d.order_id));
+                    }
+                    Ok(arr)
+                } else {
+                    Err(())
+                }
+            }
+
+            OrderManagement::Binance(_) => {
+                // TODO:  Write batch cancel for binance
+                Ok(arr)
+            }
+        }
+    }
+
+    async fn batch_place_order(&self, order_array: Vec<BatchOrder>) -> Result<Vec<LiveOrder>, ()> {
+        let order_array_clone = order_array.clone();
+        let order_arr = {
+            let mut arr = vec![];
+            for (qty, price, symbol, side) in order_array_clone {
+                arr.push(OrderRequest {
+                    category: bybit::model::Category::Linear,
+                    symbol: Cow::Owned(symbol),
+                    order_type: bybit::model::OrderType::Limit,
+                    side: {
+                        if side < 0 {
+                            bybit::model::Side::Sell
+                        } else {
+                            bybit::model::Side::Buy
+                        }
+                    },
+                    qty,
+                    price: Some(price),
+                    time_in_force: Some(Cow::Borrowed("GTC")),
+                    ..Default::default()
+                });
+            }
+            arr
+        };
+        match self {
+            OrderManagement::Bybit(trader) => {
+                let client = trader.clone();
+                let od_clone = order_array.clone();
+                let req = BatchPlaceRequest {
+                    category: bybit::model::Category::Linear,
+                    requests: order_arr,
+                };
+                if let Ok(v) = client.batch_place_order(req).await {
+                    let mut arr = vec![];
+                    for (i, d) in v.result.list.iter().enumerate() {
+                        arr.push(LiveOrder::new(
+                            od_clone[i].1.clone(),
+                            od_clone[i].0.clone(),
+                            d.order_id.to_string(),
+                        ));
+                    }
+                    Ok(arr)
+                } else {
+                    Err(())
+                }
+            }
+            OrderManagement::Binance(trader) => {
+                // TODO: Unimplemented by the crate binance
+                let client = trader.clone();
+                let order_vec = order_array.clone();
+                let order_requests = {
+                    let mut arr = vec![];
+                    for (qty, price, symbol, side) in order_vec {
+                        arr.push(CustomOrderRequest {
+                            symbol,
+                            qty: Some(qty),
+                            side: if side < 0 {
+                                OrderSide::Sell
+                            } else {
+                                OrderSide::Buy
+                            },
+                            price: Some(price),
+                            order_type: binance::futures::account::OrderType::Limit,
+                            time_in_force: Some(binance::futures::account::TimeInForce::GTC),
+                            position_side: None,
+                            stop_price: None,
+                            close_position: None,
+                            activation_price: None,
+                            callback_rate: None,
+                            working_type: None,
+                            price_protect: None,
+                            reduce_only: None,
+                        });
+                    }
+                    arr
+                };
+                let task = task::spawn_blocking(move || {
+                    if let Ok(v) = client
+                        .custom_batch_orders(order_array.len().try_into().unwrap(), order_requests)
+                    {
+                        let arr = vec![];
+                        Ok(arr)
+                    } else {
+                        Err(())
+                    }
+                });
+                task.await.unwrap()
+            }
+        }
+    }
+
+    async fn batch_amend(
+        &self,
+        orders: Vec<LiveOrder>,
+        symbol: &str,
+    ) -> Result<Vec<LiveOrder>, ()> {
+        match self {
+            OrderManagement::Bybit(trader) => {
+                let client = trader.clone();
+                let order_clone = orders.clone();
+                let req = BatchAmendRequest {
+                    category: bybit::model::Category::Linear,
+                    requests: {
+                        let mut arr = vec![];
+                        for v in orders {
+                            arr.push(AmendOrderRequest {
+                                category: bybit::model::Category::Linear,
+                                symbol: Cow::Borrowed(symbol),
+                                order_id: Some(Cow::Owned(v.order_id)),
+                                ..Default::default()
+                            });
+                        }
+                        arr
+                    },
+                };
+                if let Ok(v) = client.batch_amend_order(req).await {
+                    let mut arr = vec![];
+                    for (i, d) in v.result.list.iter().enumerate() {
+                        arr.push(LiveOrder::new(order_clone[i].price, order_clone[i].qty, d.order_id.clone().to_string()));
+                    }
+                    Ok(arr)
+                } else {
+                    Err(())
+                }
+            }
+            OrderManagement::Binance(_) => Err(()),
+        }
     }
 }
+
+// [qty, price, symbol, side] side is -1 for sell and 1 for buy
+type BatchOrder = (f64, f64, String, i32);
