d7cd1caac91b2ccf5dcf87145e0393e085a62526
unkuseni <amudalatadeoye@gmail.com>
2024-10-27 00:42:26 +0100

    update inputs

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,54 @@
 use std::collections::HashMap;
 
 use rs_smm::{parameters::parameters::use_toml, strategy::market_maker::MarketMaker};
-use skeleton::ss;
+use skeleton::{ss, util::helpers::Config};
 use tokio::sync::mpsc;
 
 // Start the program
 #[tokio::main]
 async fn main() {
     // Pull the contents of the config file
-    let config = use_toml();
+    let Config {
+        exchange,
+        symbols,
+        api_keys,
+        balances,
+        leverage,
+        orders_per_side,
+        final_order_distance,
+        update_interval,
+        depths,
+        rate_limit,
+        tick_window,
+        bps,
+    } = use_toml();
     // initialize shared state and pass in  exchange, clients, symbols
-    let mut state = ss::SharedState::new(config.exchange);
-    state.add_symbols(config.symbols);
-    let clients = config.api_keys;
+    let mut state = ss::SharedState::new(exchange);
+    state.add_symbols(symbols);
+    let clients = api_keys;
     for (key, secret, symbol) in clients {
         state.add_clients(key, secret, symbol, None);
     }
 
     // Create a hashmap for balances of each client/symbols
-    let balance = balances(config.balances);
+    let balance = map_balances(balances);
 
     // Initialize the market maker and set the initial state, balance, leverage, orders per side, final order distance, depths, and rate limit
     let mut market_maker = MarketMaker::new(
         state.clone(),
         balance,
-        config.leverage,
-        config.orders_per_side,
-        config.final_order_distance,
-        config.depths,
-        config.rate_limit,
-        config.tick_window,
-    ).await;
+        leverage,
+        orders_per_side,
+        final_order_distance,
+        update_interval,
+        depths,
+        rate_limit,
+        tick_window,
+    )
+    .await;
 
     // sets the  base spread in bps for profit
-    market_maker.set_spread_toml(config.bps);
+    market_maker.set_spread_toml(bps);
 
     // create an unbounded channel
     let (sender, receiver) = mpsc::unbounded_channel();
@@ -47,7 +62,7 @@ async fn main() {
     market_maker.start_loop(receiver).await;
 }
 
-fn balances(arr: Vec<(String, f64)>) -> HashMap<String, f64> {
+fn map_balances(arr: Vec<(String, f64)>) -> HashMap<String, f64> {
     let mut new_map = HashMap::new();
     for (k, v) in arr {
         new_map.insert(k, v);
